bc4caac8cda070b81aebd1b1649d028450f5c045
Bofamir <35529095+Bofamir@users.noreply.github.com>
2021-05-12 15:35:57 +0200

    Arbeit von Heute (2021.05.12)

---
+++ ShoppingCart.java
@@ -0,0 +1,50 @@
+package valueObject;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ShoppingCart {
+    private Map<Article, Integer> cart = new HashMap<>();
+
+    public void addToCart(Article article, int amount) {
+        if (!cart.containsKey(article)) {
+            cart.put(article, amount);
+        } else {
+            int newAmount = cart.get(article) + amount;
+            cart.put(article, newAmount);
+        }
+    }
+
+    public boolean removeFromCart(Article article, int amount) {
+        boolean succeeded = false;
+        if (!cart.containsKey(article)) {
+            // throw No Article in cart
+        } else {
+            int newAmount = cart.get(article) - amount;
+            if (!(newAmount <= 0)) {
+                cart.put(article, newAmount);
+                succeeded = true;
+            } else if (newAmount == 0) {
+                cart.remove(article);
+                succeeded = true;
+            }
+        }
+        return succeeded;
+    }
+
+    public void emptyCart() {
+        cart.clear();
+    }
+
+    public Map<Article, Integer> getWarenkorb() {
+        return cart;
+    }
+
+    public double getTotal() {
+        double total = 0;
+        for (Article article : cart.keySet()) {
+            total += article.getPrice()*cart.get(article);
+        }
+        return total;
+    }
+}
