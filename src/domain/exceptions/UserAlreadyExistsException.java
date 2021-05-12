e83b5fff2289961974e0b762caa30e11128cf308
Bofamir <35529095+Bofamir@users.noreply.github.com>
2021-05-12 16:01:17 +0200

    Arbeit von Heute (2021.05.12)

--- src/domain/exceptions/UserAlreadyExistsException.java
+++ src/domain/exceptions/UserAlreadyExistsException.java
@@ -6,5 +6,4 @@ public class UserAlreadyExistsException extends Exception {
     public UserAlreadyExistsException(User user, String msg) {
         super("User with Username: " + user.getUsername() + "already exists");
     }
-
 }
