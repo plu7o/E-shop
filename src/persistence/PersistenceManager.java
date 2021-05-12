df5f5f5917a435d588900ea990cb0ae5f949b41b
Bofamir <35529095+Bofamir@users.noreply.github.com>
2021-05-12 15:47:45 +0200

    Arbeit von Heute (2021.05.12)

--- src/persistence/PersistenceManager.java
+++ src/persistence/PersistenceManager.java
@@ -6,4 +6,12 @@ public interface PersistenceManager {
     public void openForReading(String dataSource) throws IOException;
     public void openForWriting(String dataSource) throws IOException;
     public boolean close();
+
+    public Article loadArticle() throws IOException;
+
+    public boolean saveArticle(Article article) throws IOException;
+
+    public User loadUser() throws IOException;
+
+    public boolean saveUser(User user) throws IOException;
 }
