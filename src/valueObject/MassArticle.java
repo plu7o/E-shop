296b404445ced10637feb76b047d7c3600810ef1
Bofamir <35529095+Bofamir@users.noreply.github.com>
2021-06-02 14:05:28 +0200

    Update MassArticle.java

--- src/valueObject/MassArticle.java
+++ src/valueObject/MassArticle.java
@@ -1,34 +1,88 @@
 package valueObject;
+import valueObject.Article;
 
-public class MassArticle extends Article {
-    private int packageSize;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Invoice {
+
+    private int userNr;
+    private float total = 0;
+    private String date;
+    private Map<Article, Integer> shoppingCart = new HashMap<Article, Integer>();
+    private String string;
+    private String totalStr;
     private String name;
-    private int articleNr;
-    private double price;
-    private int stock;
-    private boolean available;
-    // private boolean massArticle;
 
-    public MassArticle(String name, int articleNr, double price, int stock, boolean available, int packageSize) {
-        super(name, articleNr, price, stock, available);
-        this.packageSize = packageSize;
-        this.articleNr = articleNr;
-        this.name = name;
-        this.price = price;
-        this.stock = stock;
-        this.available = available;
-        // this.massArticle = massArticle;
+    public Invoice (User user) {
+        this.userNr = user.getUserNr();
+        this.name = user.getName();
+        this.shoppingCart = user.getShoppingCart().getCart();
+        LocalDateTime now = LocalDateTime.now();
+        DateTimeFormatter format = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        this.date = format.format(now);
     }
+
+    /**
+     * Text
+     * @return
+     */
     public String toString() {
-        String availability = this.available ? "in stock" : "SOLD OUT";
-        return ("Nr: " + articleNr + " | Article-name: " + name + " | Price: " + price + "€" + " | Availability: " + availability + " | Package Size: " + packageSize );
+        double price = 0;
+        String str = "";
+        str += "+----------INVOICE----------+\n";
+        str += "| Date: " + date + "          |\n";
+        str += "| UserID: " + userNr + String.join("", Collections.nCopies(18 - String.valueOf(userNr).length(), " ")) + "|\n";
+        str += "| name: " + name + String.join("", Collections.nCopies(20 - name.length(), " ")) + "|\n";
+        str += "+---------------------------+\n";
+
+        String priceStr = "";
+        String articleName = "";
+        int articleAmount = 0;
+        int spaceAmount = 0;
+        String spaces = "";
+        for (Article article : shoppingCart.keySet()) {
+            price = article.getPrice() * shoppingCart.get(article);
+            priceStr = turnToEuro(price);
+            articleName = article.getName();
+            articleAmount = shoppingCart.get(article);
+            spaceAmount = 23 - priceStr.length() - articleName.length() - String.valueOf(articleAmount).length();
+            if (spaceAmount >= 0) { spaces = String.join("", Collections.nCopies(spaceAmount, " ")); }
+            else                  { spaces = ""; }
+            str += "| " + articleName + " x" + articleAmount + spaces + priceStr + " |\n";
+            total += price;
+        }
+
+        this.totalStr = turnToEuro(total);
+        str += "+---------------------------+\n";
+        str += "| Summe: " + totalStr + String.join("", Collections.nCopies(18 - totalStr.length(), " ")) + " |\n";
+        str += "+---------------------------+\n";
+        str += "\n";
+        str += "Danke für Ihren einkauf!\n";
+        return str;
+    }
+
+    public void print() {
+        System.out.println(string);
+    }
+
+    private String turnToEuro(double price) {
+        String str = (int)price + ",";
+        int afterComma = (int)(price*100-(int)price*100);
+        if (afterComma == 0)      { str += "00"; }
+        else if (afterComma < 10) { str += afterComma + "0"; }
+        else                      { str += afterComma; }
+        str += "€";
+        return str;
     }
 
     //Getter
-    public int getPackageSize() { return packageSize; }
-    //public boolean isMassArticle() { return massArticle; }
+    public float getTotal() { return total; }
 
-    //Setter
-    public void setPackageSize  (int packageSize) {this.packageSize = packageSize; }
-    //public void setMassArticle(boolean massArticle)   { this.massArticle = massArticle; }
+    public String getString() { return string; }
+
+    public String getTotalString() { return totalStr; }
 }
